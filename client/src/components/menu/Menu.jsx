74252fd3f13b12cc926f77f4551b2402b36dfb9c
MohamedTaha-01 <mohamedtaha.mt01@gmail.com>
2023-01-11 20:02:22 +0100

    fetch menu from server

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -27,294 +27,26 @@ export default function Menu() {
     },
   ];
   const [currentTab, setCurrentTab] = useState(0);
-  const [foodItems, setFoodItems] = useState([
-    {
-      type: "burger",
-      name: "Clásica",
-      description:
-        "La hamburguesa clásica es una opción segura para cualquier amante de las hamburguesas. Con una generosa porción de carne de res, queso cheddar fundido, lechuga fresca, tomate fresco y nuestra deliciosa salsa especial, ¡es una opción deliciosa para cualquier ocasión!",
-      ingredients: [
-        "Carne de res",
-        "Queso cheddar",
-        "Lechuga",
-        "Tomate",
-        "Salsa especial",
-      ],
-      price: 6.95,
-    },
-    {
-      type: "burger",
-      name: "Big Burger",
-      description:
-        "La hamburguesa Big Burger es un monstruo de carne con una generosa porción de carne de res, queso cheddar fundido, tomate fresco, lechuga crujiente y cebolla caramelizada. ¡Todos los ingredientes se combinan a la perfección en este plato que es una explosión de sabor en cada bocado!",
-      ingredients: [
-        "Carne de res",
-        "Queso cheddar",
-        "Tomate",
-        "Lechuga",
-        "Cebolla",
-      ],
-      price: 8.95,
-    },
-    {
-      type: "burger",
-      name: "Veggie Burger",
-      description:
-        "Nuestra hamburguesa vegetariana, la Veggie Delight, está hecha con una deliciosa mezcla de verduras frescas y proteína vegetal. Cubierta con queso feta y aderezada con nuestra salsa especial de ajo y hierbas, ¡está seguro de satisfacer a cualquier amante de la comida vegetariana!",
-      ingredients: [
-        "Mezcla de verduras frescas (como zanahorias ralladas, espinacas y champiñones)",
-        "Proteína vegetal (como frijoles o tofu)",
-        "Queso feta",
-        "Salsa de ajo y hierbas",
-      ],
-      price: 7.95,
-    },
-    {
-      type: "burger",
-      name: "BBQ bacon",
-      description:
-        "La hamburguesa BBQ Bacon es una explosión de sabor ahumado y dulce. Con una generosa porción de tocino ahumado, queso cheddar fundido y nuestra deliciosa salsa BBQ, ¡está seguro de convertirse en tu nueva hamburguesa favorita!",
-      ingredients: [
-        "Carne de res",
-        "Tocino ahumado",
-        "Queso cheddar",
-        "Salsa BBQ",
-      ],
-      price: 9.95,
-    },
-    {
-      type: "burger",
-      name: "El As de la Carne",
-      description:
-        "Hamburguesa con dos gruesas capas de carne de alta calidad, lechuga fresca, tomate fresco, tiras crujientes de bacon y nuestra deliciosa salsa de ajo y queso casera. ¡Una opción de lujo para los verdaderos amantes de la carne!",
-      ingredients: [
-        "Carne de res",
-        "Lechuga",
-        "Tomate",
-        "Bacon",
-        "Salsa de ajo y queso casera",
-      ],
-      price: 11.95,
-    },
-    {
-      type: "burger",
-      name: "El Smash King",
-      description:
-        "Hamburguesa con carne estilo smash, cebolla a la plancha, tiras crujientes de bacon y nuestro especial aderezo de tomate y cebolla. ¡Un sabor espectacular que hará reinar tu paladar!",
-      ingredients: [
-        "Carne de res al estilo smash",
-        "Cebola",
-        "Bacon",
-        "Aderezo de tomate y cebolla",
-      ],
-      price: 9.95,
-    },
-    {
-      type: "burger",
-      name: "Pollo a la parrilla",
-      description:
-        "La hamburguesa de pollo a la parrilla es una opción más ligera para aquellos que buscan una opción más saludable. Con una pechuga de pollo a la parrilla y aderezada con nuestra salsa especial de ajo y hierbas, ¡es una opción deliciosa para cualquier ocasión!",
-      ingredients: [
-        "Pechuga de pollo a la parrilla",
-        "Queso cheddar",
-        "Lechuga",
-        "Tomate",
-        "Salsa de ajo y hierbas",
-      ],
-      price: 7.95,
-    },
-    {
-      type: "burger",
-      name: "Pollo picante",
-      description:
-        "La hamburguesa de pollo picante es para aquellos con un paladar más atrevido. Con pechuga de pollo marinada en nuestra salsa picante especial y cubierta con queso cheddar fundido, ¡está seguro de ser un plato popular!",
-      ingredients: [
-        "Pechuga de pollo marinada en salsa picante especial",
-        "Queso cheddar",
-        "Lechuga",
-        "Tomate",
-        "Cebolla",
-      ],
-      price: 8.45,
-    },
-    {
-      type: "burger",
-      name: "La Huevo-matic",
-      description:
-        "¡Pon un poco de sorpresa en tu hamburguesa con La Huevo-matic! Esta deliciosa hamburguesa viene con un huevo fresco y crujiente encima de nuestra carne de alta calidad y verduras frescas. El huevo se cocina a la perfección en nuestra parrilla de carbón para darle ese delicioso sabor ahumado. ¡Ven a probar La Huevo-matic hoy mismo en The Burger House y sorprende a tus amigos con esta deliciosa opción de hamburguesa con huevo! La Huevo-matic es una hamburguesa que te dejará sin aliento!",
-      ingredients: [
-        "Carne de res",
-        "Huevo",
-        "Bacon",
-        "Tomate",
-        "Cebolla frita",
-        "Mayonesa",
-      ],
-      price: 8.95,
-    },
-    {
-      type: "burger",
-      name: "Infantil",
-      description:
-        "Una hamburguesa pequeña hecha con carne de alta calidad y verduras frescas. ¡Ven a probar nuestro menú infantil hoy mismo en The Burger House y déjate sorprender por la sonrisa de tus pequeños!",
-      ingredients: ["Carne de res", "Lechuga", "Tomate", "Mayonesa"],
-      price: 5.45,
-    },
-    {
-      type: "side",
-      name: "Patatas M",
-      description:
-        "Nuestras patatas fritas crujientes y deliciosas son el acompañamiento perfecto para cualquiera de nuestras hamburguesas o para disfrutar solas. ¡Ven a probar nuestras patatas fritas hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 2,
-    },
-    {
-      type: "side",
-      name: "Patatas XL",
-      description:
-        "Nuestras patatas fritas crujientes y deliciosas son el acompañamiento perfecto para cualquiera de nuestras hamburguesas o para disfrutar solas. ¡Ven a probar nuestras patatas fritas hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 2.75,
-    },
-    {
-      type: "side",
-      name: "Patatas gajo M",
-      description:
-        "Nuestras patatas de gajo son el acompañamiento perfecto para cualquiera de nuestras hamburguesas o para disfrutar solas. ¡Ven a probar nuestras patatas de gajo hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 2,
-    },
-    {
-      type: "side",
-      name: "Patatas gajo XL",
-      description:
-        "Nuestras patatas de gajo son el acompañamiento perfecto para cualquiera de nuestras hamburguesas o para disfrutar solas. ¡Ven a probar nuestras patatas de gajo hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 2.75,
-    },
-    {
-      type: "side",
-      name: "Cubo de nuggets (12 Uds.)",
-      description:
-        "Nuestro cubo de nuggets es la opción perfecta para los amantes de la comida rápida. Estos nuggets crujientes y deliciosos están hechos con pollo de alta calidad y se fríen a la perfección para darles ese sabor crujiente y delicioso. ¡Ven a probar nuestro cubo de nuggets hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 6.5,
-    },
-    {
-      type: "side",
-      name: "Cubo de alitas (8 Uds.)",
-      description:
-        "Nuestro cubo de alitas es la opción perfecta para los amantes de la comida rápida. Estas alitas crujientes y deliciosas están hechas con pollo de alta calidad y se fríen a la perfección para darles ese sabor crujiente y delicioso. ¡Ven a probar nuestro cubo de alitas hoy mismo en The Burger House!",
-      ingredients: [],
-      price: 8.5,
-    },
-    {
-      type: "side",
-      name: "Ensalada",
-      description:
-        "Nuestra ensalada fresca y crujiente es la opción perfecta para aquellos que buscan algo ligero y saludable. Esta ensalada viene con una mezcla de verduras frescas, como lechuga, tomate y pepino, y se sirve con nuestra deliciosa vinagreta casera. También puedes añadir proteína a tu ensalada con nuestras opciones de pollo o tofu. ¡Ven a probar nuestra ensalada hoy mismo en The Burger House y siente el poder de la verdura fresca!",
-      ingredients: [],
-      price: 6,
-    },
-    {
-      type: "drink",
-      name: "Coca Cola",
-      description: "",
-      ingredients: [],
-      price: 2.95,
-    },
-    {
-      type: "drink",
-      name: "Coca Cola Zero",
-      description: "",
-      ingredients: [],
-      price: 2.95,
-    },
-    {
-      type: "drink",
-      name: "Fanta",
-      description: "",
-      ingredients: [],
-      price: 2.95,
-    },
-    {
-      type: "drink",
-      name: "Fanta Zero",
-      description: "",
-      ingredients: [],
-      price: 2.95,
-    },
-    {
-      type: "drink",
-      name: "Nestea",
-      description: "",
-      ingredients: [],
-      price: 3,
-    },
-    {
-      type: "drink",
-      name: "Agua",
-      description: "",
-      ingredients: [],
-      price: 1.5,
-    },
-    {
-      type: "dessert",
-      name: "Sundae de helado",
-      description: "Una opción clásica y siempre popular",
-      ingredients: [],
-      price: 5,
-    },
-    {
-      type: "dessert",
-      name: "Galletas y helado",
-      description:
-        "Una opción más ligera que el sundae, pero igualmente deliciosa.",
-      ingredients: [],
-      price: 5,
-    },
-    {
-      type: "dessert",
-      name: "Batido",
-      description: "Una opción fresca y ligera para los días calurosos",
-      ingredients: [],
-      price: 5,
-    },
-    {
-      type: "dessert",
-      name: "Tiramisú",
-      description:
-        "Capas de bizcocho empapado en café, crema de mascarpone y cacao en polvo.",
-      ingredients: [],
-      price: 5,
-    },
-    {
-      type: "sauce",
-      name: "Ketchup",
-      description:
-        "¡No te pierdas nuestro ketchup casero con un toque de especias para acompañar tus patatas",
-      ingredients: [],
-      price: 1.2,
-    },
-    {
-      type: "sauce",
-      name: "Salsa barbacoa",
-      description:
-        "¡Lleva tus acompañantes al siguiente nivel con nuestra deliciosa salsa barbacoa casera!",
-      ingredients: [],
-      price: 1.5,
-    },
-    {
-      type: "sauce",
-      name: "Salsa de ajo y hierbas",
-      description: "",
-      ingredients: [],
-      price: 1.5,
-    },
-  ]);
+  const [foodItems, setFoodItems] = useState("");
 
+  // GET MENU ITEMS FROM SERVER
   useEffect(() => {
-    // fetch
+    fetch("http://localhost:3000/api/menu", {
+      method: "GET",
+      "Content-type": "application/json",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFoodItems(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
 
   return (
@@ -395,7 +127,7 @@ export default function Menu() {
             else return null;
           })
         ) : (
-          <p>Error 500</p>
+          <p>...</p>
         )}
       </div>
     </section>
